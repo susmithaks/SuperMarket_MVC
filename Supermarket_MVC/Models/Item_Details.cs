dbc78f81fbcebb48b0f846f3c7f1a8a41ccb1ebc
Susmitha <Susmithasundar31@gmail.com>
2022-07-19 13:21:45 +0530

    Added Item Details.

---
+++ Supermarket_MVC/Models/Item_Details.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Supermarket_MVC.Models
+{
+    public class Item_Details
+    {   //primary key.
+        [Key]
+        //Auto increment
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+       
+
+    }
+}
