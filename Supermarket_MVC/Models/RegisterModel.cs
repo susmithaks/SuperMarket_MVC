56d967124566b7712aaf7a091416d7042c63f0c4
Srivatshan <srirenga96@gmail.com>
2022-07-19 12:11:46 +0530

    Register model DataAnnotation changes.

--- Supermarket_MVC/Models/RegisterModel.cs
+++ Supermarket_MVC/Models/RegisterModel.cs
@@ -15,10 +15,16 @@ namespace Supermarket_MVC.Models
         //Auto increment
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+        [StringLength (10)]
         public String Name { get; set; }
+        [EmailAddress(ErrorMessage ="Enter a Valid Email ID")]
         public String Email_ID { get; set; }
+        [Required]
         public int Age { get; set; }
+        [Required]
+        [MinLength (4,ErrorMessage = "Enter minimum 4 character")]
         public string  UserName { get; set; }
+        [Required]
         public string Password { get; set; }
 
     }
